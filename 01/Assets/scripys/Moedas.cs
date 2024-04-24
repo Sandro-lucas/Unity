d72896afaccedf4ceb4d19b9a98423e766d54a14
Sandro-lucas <152908944+Sandro-lucas@users.noreply.github.com>
2024-04-23 23:09:17 -0300

    moedas_e_som

---
+++ 01/Assets/scripys/Moedas.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moedas : MonoBehaviour
+{
+    
+     void Start()
+    {
+        FindObjectOfType<GameManager>().moedasNaFase += 1;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+           FindObjectOfType<GameManager>().DescontarMoedas();
+         Destroy(this.gameObject);
+        }
+    }
+}
